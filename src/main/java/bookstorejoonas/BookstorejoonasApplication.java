f13daf5d7ec7b790ae3101b39f7902f12cc420a6
Joomas R <ristikartanoj@gmail.com>
2026-09-01 13:17:28 +0300

    Vaihdoin hakemistojen nimiä

--- src/main/java/bookstorejoonas/BookstorejoonasApplication.java
+++ src/main/java/bookstorejoonas/BookstorejoonasApplication.java
@@ -1,4 +1,4 @@
-package com.example.bookstorejoonas;
+package bookstorejoonas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/bookstorejoonas/web/BookController.java
+++ src/main/java/bookstorejoonas/web/BookController.java
@@ -1,4 +1,4 @@
-package com.example.bookstorejoonas.web;
+package bookstorejoonas.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
