d8b69eebe923b0c778da0ff2b649034908be5e8c
Joomas R <ristikartanoj@gmail.com>
2026-09-14 17:49:44 +0300

    Add JPA and database

--- src/main/java/bookstorejoonas/BookstorejoonasApplication.java
+++ src/main/java/bookstorejoonas/BookstorejoonasApplication.java
@@ -1,13 +1,29 @@
 package bookstorejoonas;
 
+import java.beans.BeanProperty;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import bookstorejoonas.domain.Book;
+import bookstorejoonas.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstorejoonasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstorejoonasApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+		repository.save(new Book("Tuntematon sotilas", "Väinö Linna", 1954, "978-951-0-36530-4", 14.99));
+		repository.save(new Book("Tunnettu sotilas", "Jari Sarasvuo", 1253, "822-351-0-36530-4", 19.99));
+		};
+	
 	}
 
 }
--- src/test/java/bookstorejoonas/BookstorejoonasApplicationTests.java
+++ src/test/java/bookstorejoonas/BookstorejoonasApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.bookstorejoonas;
+package bookstorejoonas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
